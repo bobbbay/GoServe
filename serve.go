c89888f6ceae4c0997d8ba61d858af76377c9085
Bobbay <abatterysingle@gmail.com>
2020-02-03 23:03:39 +0000

    Fixed. Everything

--- serve.go
+++ serve.go
@@ -5,6 +5,9 @@ import (
   "net/http"
   "io/ioutil"
   "strings"
+  "bufio"
+  "os"
+  "strconv"
 )
 
 func ignore(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +23,66 @@ func banner() {
 	fmt.Println("-----------------------------------------------------------------------------------------------------------------")
 }
 
-func main() {
+func clean(){
+	os.Remove("goserve.conf")
+	os.Create("goserve.conf")
+	fmt.Println("GoServe cleaned, run make to start again")
+}
+
+func setup() {
+
+  reader := bufio.NewReader(os.Stdin)
+  banner()
+
+  steps := []string{"Port", "Source (starting from ..)", "Ignore Dir (starting from ..)"}
+
+  for i:=0; i<len(steps);i++{
+	fmt.Print(steps[i] + " $ ")
+	if i == 1 {
+		fmt.Print("../")
+	}
+    text, _ := reader.ReadString('\n')
+    // convert CRLF to LF
+	text = strings.Replace(text, "\n", "", -1)
+
+	num, err := strconv.Atoi(text)
+	if err == nil && num <= 1024 {
+		fmt.Println("We don't support low ports yet")
+		steps = append([]string{"Prepend Item"}, steps...)
+	}
+
+	// At this point, we consider all is good
+	settings, err := os.OpenFile("goserve.conf", os.O_APPEND|os.O_WRONLY, 0644)
+	
+	if err != nil {
+        fmt.Println(err)
+        return
+    }
+	
+	write, err := settings.WriteString(text + ";")
+	fmt.Println(write)
+
+	if err != nil {
+        fmt.Println(err)
+        settings.Close()
+        return
+	}
+		
+	err = settings.Close()
+	
+	if err != nil {
+        fmt.Println(err)
+        return
+	}
+
+  }
+
+  fmt.Println("All done setting up!")
+  serve()
+
+}
+
+func serve() {
   banner()
 
   data, err := ioutil.ReadFile("goserve.conf")
@@ -42,5 +104,32 @@ func main() {
   fmt.Println("Server is up and running, serving " + srcDir + ", ignoring " + ignoreDir + ".")
   if err := http.ListenAndServe(":" + port, nil); err != nil {
     panic(err)
+  }
+}
+
+func main(){
+  confLen, _ := os.Stat("goserve.conf")
+  conf := confLen.Size()
+  nilFile, _ := os.Stat("nilFile.goserve")
+  nilLen := nilFile.Size()
+
+  if conf == nilLen {
+    setup()
+  } else {
+	fmt.Print("Would you like to restart? ")
+	reader := bufio.NewReader(os.Stdin)
+    text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+	if text == "y" {
+	  fmt.Print("Are you sure? ")
+	  reader2 := bufio.NewReader(os.Stdin)
+      text2, _ := reader2.ReadString('\n')
+	  text2 = strings.Replace(text2, "\n", "", -1)
+	  if text2 == "y" {
+		clean()
+	  }
+	} else if text == "n" {
+		serve()
+	}
   }
 }
