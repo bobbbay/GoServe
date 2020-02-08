8f2979b62c9204dad0346075d1a912e2f8917372
Bobbay <abatterysingle@gmail.com>
2020-02-08 03:06:02 +0000

    installer stuff

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 binary=`uname -m`
 
-curl --output GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}"
+git clone https://github.com/Bobbbay/GoServe.git
+curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}"
