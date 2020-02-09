e36d0210aaa20745436ead862944c592c9cf8505
Bobbay <abatterysingle@gmail.com>
2020-02-09 22:11:55 +0000

    Binary release installation system ready

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 binary=`uname -m`
 
-git clone https://github.com/Bobbbay/GoServe.git && curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}" && rm -rf ./GoServe/makefile && curl --ouput ./GoServe/makefile "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/makefile"
+git clone https://github.com/Bobbbay/GoServe.git && curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}" && rm -rf ./GoServe/makefile && curl --output ./GoServe/makefile "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/makefile"
