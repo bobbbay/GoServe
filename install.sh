220ee0d688a4e0607a958c4fac1868596a1e44e7
Bobbay <49123594+Bobbbay@users.noreply.github.com>
2020-02-16 12:25:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 binary=`uname -m`
 
-git clone https://github.com/Bobbbay/GoServe.git && curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}" && cd ./GoServe && chmod u+x ./GoServe
+git clone https://github.com/Bobbbay/GoServe.git && curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}" && cd ./GoServe && chmod u+x ./GoServe && rm -rf makefile && wget https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/makefile && cd ..
