0b487ac41348b833a3d5657427cdb4ad9c55579e
Bobbbay <abatterysingle@gmail.com>
2020-02-16 17:11:50 +0000

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 binary=`uname -m`
 
-git clone https://github.com/Bobbbay/GoServe.git && curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}" && cd ./GoServe && chmod u+x
+git clone https://github.com/Bobbbay/GoServe.git && curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}" && cd ./GoServe && chmod u+x ./GoServe
