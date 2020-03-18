5883f8d12e7e9192080df5bcdf3cc206c8278ea2
Bobbbay <abatterysingle@gmail.com>
2020-03-18 17:31:35 +0000

    installation help

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 binary=`uname -m`
+dir=$PWD
 
-git clone https://github.com/Bobbbay/GoServe.git && curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}" && cd ./GoServe && chmod u+x ./GoServe && rm -rf makefile && wget https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/makefile && cd ..
+git clone https://github.com/Bobbbay/GoServe.git && curl --output ./GoServe/GoServe "https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/GoServe-${binary}" && cd ./GoServe && chmod u+x ./GoServe && rm -rf makefile && wget https://raw.githubusercontent.com/Bobbbay/GoServe/master/binaries/makefile && cd .. && echo export PATH="{dir}" + $PATH >> .bashrc
